bcbe7b250b845b4f9bed25736d3882cba29245db
2021Abdi <76954538+2021Abdi@users.noreply.github.com>
2021-01-08 10:27:53 +0100

    Update Calculator.cs

--- C# Projekt/Calculator.cs
+++ C# Projekt/Calculator.cs
@@ -8,7 +8,7 @@ namespace p
         public double Tid { get; set; }
         public double Stracka { get; set; }
 
-        public void Hastighet()
+        public void Velocity()
         {
             Boolean tryAgain;
             do
@@ -42,7 +42,7 @@ namespace p
                 }
             } while (tryAgain == true);
         }
-        public void Tid()
+        public void Time()
         {
             Boolean tryAgain;
             do
@@ -76,7 +76,7 @@ namespace p
                 }
             } while (tryAgain == true);
         }
-        public void Stracka()
+        public void Distance()
         {
             Boolean tryAgain;
             do
